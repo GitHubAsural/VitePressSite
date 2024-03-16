8c6d772ff890527d7b4d28f6deed25168f3ab193
SoloAsural <WZ9459@163.com>
2024-03-16 09:44:23 +0800

    ee

--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,29 @@
-#!/usr/bin/env  sh
+#!/usr/bin/env bash
 
 set -e
 
-# npm run docs:build
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
 
-# cd docs/.vitepress/dist
+echo -e "${RED}请输入git commit内容！${RESET}"
 
-# 输出标题
-echo "请输入git commit内容！"
+default_commit="Github Action 自动部署. $(date +"%Y-%m-%d %H:%M:%S")"
 
-# 显示默认提示
-default_commit="Github Action 自动部署. "$(date +"%Y-%m-%d %H:%M:%S")
+read -p "(回车使用默认提交信息：$default_commit): " msg
 
-# 读取用户输入
-read -p "（回车使用默认提交信息：$default_commit）:" msg
+msg="${msg:-$default_commit}"
 
-# 如果用户输入为空，则使用默认提交信息
-if [[ -z "$msg" ]]; then
-  msg=$default_commit
-fi
-
-# 判断是否已经执行过git init
-if [[ -d .git ]]; then
-  echo "本地已经执行过git init，不再执行。"
-else
-  # 执行git init
+if [ !-d .git ]; then
   git init
+else
+  echo "本地已经执行过git init，不再执行。"
 fi
 
 git add -A
 
-git commit -m "${msg}"
+git commit -m "$msg"
 
 # git push -v -f git@github.com:GitHubAsural/VitePressSite.git
 
-# cd -
-# rm -rf docs/.vitepress/dist node_modules docs/.vitepress/cache
 rm -rf docs/.vitepress/dist docs/.vitepress/cache
